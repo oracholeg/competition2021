b82406e2b74490478b10c87a17297991fb416571
oracholeg <83121949+oracholeg@users.noreply.github.com>
2024-09-16 10:57:40 +0300

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
 	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 
-	$mysql = new mysqli('localhost', 'root', 'root', 'register-bd');
+	$mysql = new mysqli('dpg-crju1njtq21c73a6f43g-a', 'users_6iez_user', 'dHgvEXPu5gezKXJ70CbD4cW5zXKonRZ3', 'user-db');
 	$mysql->query("INSERT INTO `users` (`login`, `pass`) VALUES('$login', '$pass')");
 	$mysql-> close();
 
